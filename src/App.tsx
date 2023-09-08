843ae406f166d9c8a4145532eba4b0d5bce3436d
Grzegorz Marszalek <grzegorz.marszalek@sidnet.pl>
2023-09-08 10:21:46 +0200

    Add HashRouter instead of BroweserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import Accountant from "./pages/AccountantPage"
@@ -6,13 +6,13 @@ import RootLayout from "./components/root-layout/root-layout"
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <RootLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/ksiegowi" element={<Accountant />} />
         </Routes>
       </RootLayout>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
